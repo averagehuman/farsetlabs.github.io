53d1181541b3cfa85052ba2bd08991b61b6f4f11
averagehuman <cyclebelfast@gmail.com>
2018-01-30 15:04:00 +0000

    [voltaic-theme] Initial gulp/sass/jekyll setup.
    A gulpfile based on the zurb foundation basic template together with a
    jekyll task to build the site itself.

---
+++ gulpfile.babel.js
@@ -0,0 +1,133 @@
+'use strict';
+
+import plugins       from 'gulp-load-plugins';
+import yargs         from 'yargs';
+import browser       from 'browser-sync';
+import gulp          from 'gulp';
+import log           from 'fancy-log';
+import rimraf        from 'rimraf';
+import child_process         from 'child_process';
+
+// Load all Gulp plugins into one variable
+const $ = plugins();
+
+// Your project's server will run on localhost:xxxx at this port
+const PORT = 8000;
+
+// Autoprefixer will make sure your CSS works with these browsers
+const COMPATIBILITY = [
+  "last 2 versions",
+  "ie >= 9",
+  "ios >= 7"
+];
+
+// UnCSS will use these settings
+const UNCSS_OPTIONS = {
+  html: "src/**/*.html",
+  ignore: [
+    /.foundation-mq/,
+    /^\.is-.*/,
+  ]
+};
+
+// Gulp will reference these paths when it copies files
+const PATHS = {
+  // Path to source folder
+  src: "_sass",
+  // Path to dest folder
+  dest: "assets/css",  
+  // Paths to Sass libraries, which can then be loaded with @import
+  includePaths: [
+    "node_modules/foundation-sites/scss",
+    "node_modules/motion-ui/src"
+  ],
+  build: "_site"
+};
+
+// Pin jekyll version to that used by github pages - https://pages.github.com/versions/
+const JEKYLL_VERSION = "3.6.2";
+
+const PWD = process.cwd();
+
+const JEKYLL_BUILD_CMD = [
+  'docker',
+  'run',
+  '-v',
+  `${PWD}:/srv/jekyll`,
+  '-v',
+  `${PWD}/.cache/bundle:/usr/local/bundle`,
+  '-it',
+  `jekyll/jekyll:${JEKYLL_VERSION}`,
+  'jekyll',
+  'build'
+];
+
+// Check for --production flag
+const PRODUCTION = !!(yargs.argv.production);
+
+
+//-------------------------------------------------------------------------------------------------
+// Gulp tasks
+//-------------------------------------------------------------------------------------------------
+
+// Build the "dest" folder by running all of the below tasks
+gulp.task('build',
+ gulp.series(clean, sass, jekyll));
+
+// Build the site, run the server, and watch for file changes
+gulp.task('default',
+  gulp.series('build', server, watch));
+
+// Delete the "build" folder
+// This happens every time a build starts
+function clean(done) {
+  rimraf(PATHS.build, done);
+}
+
+// Compile Sass into CSS
+// In production, the CSS is compressed
+function sass() {
+  return gulp.src(PATHS.src + '/app.scss')
+    .pipe($.sourcemaps.init())
+    .pipe($.sass({
+      includePaths: PATHS.includePaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: COMPATIBILITY
+    }))
+    // Comment in the pipe below to run UnCSS in production
+    //.pipe($.if(PRODUCTION, $.uncss(UNCSS_OPTIONS)))
+    .pipe($.if(PRODUCTION, $.cleanCss({ compatibility: 'ie9' })))
+    .pipe($.if(!PRODUCTION, $.sourcemaps.write()))
+    .pipe(gulp.dest(PATHS.dest))
+    .pipe(browser.reload({ stream: true }));
+}
+
+function jekyll() {
+  var proc = child_process.spawn(JEKYLL_BUILD_CMD[0], JEKYLL_BUILD_CMD.slice(1), {stdio: 'inherit'});//
+  proc.on('data', (data) => log.info(data));
+  //proc.on('error', (error) => log.error(error));
+  //proc.on('close', () => log.info("done jekyll"));
+  return proc;
+}
+
+// Start a server with BrowserSync to preview the site in
+function server(done) {
+  browser.init({
+    server: PATHS.build, port: PORT
+  });
+  done();
+}
+
+// Reload the browser with BrowserSync
+function reload(done) {
+  browser.reload();
+  done();
+}
+
+// Watch for changes to static sass files
+function watch() {
+  gulp.watch(PATHS.src + '/**/*.scss').on('all', sass);
+  gulp.watch(`{${PWD}/**/*.md,${PWD}/assets/**/*}`).on('all', gulp.series(jekyll, browser.reload));
+}
