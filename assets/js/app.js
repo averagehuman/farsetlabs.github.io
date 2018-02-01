7dd5d4e7f3110910af7b1d1cb0ea6903cc07365e
averagehuman <cyclebelfast@gmail.com>
2018-02-01 14:43:16 +0000

    [voltaic-theme] Work on the front page carousel.
    Replaced the existing image carousel with Foundation orbit. Looks ok in
    chrome but there is an issue in Firefox where each image in turn is
    briefly replaced with an empty white background before the next image is
    shown. Maybe look at other carousel libraries.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
-$(document).foundation()
+/*
+(function() {
+  var captions = $('.carousel-caption'), caption;
+  if (captions && captions.length && captions.length > 0) {
+    $('.orbit-figure').each(function(idx) {
+      if (idx < captions.length) {
+        caption = captions[idx];
+      } else {
+        caption = captions[captions.length-1];
+      }
+      console.log(caption);
+      $(this).append('<div class="orbit-caption"><div class="row">' + $(caption).html() + '</div></div>');
+    });
+  }
+        
+})();
+*/
+$(document).foundation();
