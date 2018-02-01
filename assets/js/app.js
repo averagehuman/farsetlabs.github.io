24adc4a103089403605cffb423dc69dcc1befe55
averagehuman <cyclebelfast@gmail.com>
2018-02-01 22:04:01 +0000

    [voltaic-theme] Move back to flexslider for the image carousel

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,18 +1,34 @@
-/*
-(function() {
-  var captions = $('.carousel-caption'), caption;
-  if (captions && captions.length && captions.length > 0) {
-    $('.orbit-figure').each(function(idx) {
-      if (idx < captions.length) {
-        caption = captions[idx];
-      } else {
-        caption = captions[captions.length-1];
-      }
-      console.log(caption);
-      $(this).append('<div class="orbit-caption"><div class="row">' + $(caption).html() + '</div></div>');
-    });
-  }
-        
-})();
-*/
-$(document).foundation();
+
+$(document).ready(function() {
+
+  // Foundation initialisation
+  $(document).foundation();
+
+  // Flexslider - add captions dynamically
+  (function() {
+    var captions = $('.carousel-caption'), caption;
+    if (captions && captions.length && captions.length > 0) {
+      $('.flexslider .slides > li').each(function(idx) {
+        if (idx < captions.length) {
+          caption = captions[idx];
+        } else {
+          caption = captions[captions.length-1];
+        }
+        console.log(caption);
+        $(this).append('<div class="flex-caption"><div class="row">' + $(caption).html() + '</div></div>');
+      });
+    }
+          
+  })();
+
+  // Flexslider initialisation
+  $('.flexslider').flexslider({
+    animation: "fade",
+    controlNav: false,
+    directionNav: true,
+    slideshowSpeed: 8000,
+    animationSpeed: 800,
+    touch: true
+  });
+});
+
